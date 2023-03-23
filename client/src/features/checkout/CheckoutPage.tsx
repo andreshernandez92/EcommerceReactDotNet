10e5deb2e7ff81d060b0b692ff26c29ce4c38cd2
andreshernandez92 <ajhl1992@gmail.com>
2023-03-23 17:27:16 -0600

    Update with Checkoutpages

--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -1,10 +1,92 @@
-import { Typography } from "@mui/material";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AddressForm from './AddressForm';
+import PaymentForm from './PaymentForm';
+import Review from './Review';
+import { FormProvider, useForm } from 'react-hook-form';
+import { FieldValues } from 'react-hook-form/dist/types/fields';
 
-export default function CheckoutPage(){
-return(
-    <Typography variant='h3'>
-        Only logged in users should be able to see this!
-    </Typography>
-)
+const steps = ['Shipping address', 'Review your order','Payment details'];
 
+function getStepContent(step: number) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+        return <Review />;
+    case 2:
+        return <PaymentForm />;
+    default:
+      throw new Error('Unknown step');
+  }
+}
+
+
+export default function CheckoutPage() {
+    const methods = useForm();
+    const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = (data: FieldValues) => {
+    if(activeStep === 0){
+    console.log(data)
+}
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  return (
+ <FormProvider {...methods}>   
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography variant="h5" gutterBottom>
+                Thank you for your order.
+              </Typography>
+              <Typography variant="subtitle1">
+                Your order number is #2001539. We have emailed your order
+                confirmation, and will send you an update when your order has
+                shipped.
+              </Typography>
+            </React.Fragment>
+          ) : (
+            <form onSubmit={methods.handleSubmit(handleNext)}>
+              {getStepContent(activeStep)}
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {activeStep !== 0 && (
+                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Back
+                  </Button>
+                )}
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                </Button>
+              </Box>
+            </form>
+          )}
+        </Paper>
+        </FormProvider>
+  );
 }
