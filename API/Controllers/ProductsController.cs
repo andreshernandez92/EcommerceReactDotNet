805b7440f765a9a25f91952c7774342d70b49e13
andreshernandez92 <ajhl1992@gmail.com>
2023-10-12 01:15:51 -0600

    Some changes

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -70,6 +70,38 @@ namespace API.Controllers
         return BadRequest(new ProblemDetails {Title = "Problem creating a new product"});
 
         }
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
+        {
+            var product = await _context.Products.FindAsync(productDto.Id);
+
+            if (product == null) return NotFound();
+
+            _mapper.Map(productDto, product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok(product);
+
+            return BadRequest(new ProblemDetails { Title = "Problem updating product" });
+        }
+               [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            _context.Products.Remove(product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest(new ProblemDetails { Title = "Problem deleting product" });
+        }
 
 
     }
---
+++ API/DTOs/UpdateProductDto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile File { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        [Range(0,500)]
+        public int QuantityStock { get; set; }  
+    }
+}
