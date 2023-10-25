056696d4b861bff7615bcd714b9fb79ea05e85ec
andreshernandez92 <ajhl1992@gmail.com>
2023-10-24 22:40:51 -0600

    Fixed the problem with Brand, Ordering and Types

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -7,7 +7,9 @@ import RadioButtonGroup from "../../app/components/RadioButton";
 import CheckboxButtons from "../../app/components/CheckboxButtons";
 import AppPagination from "../../app/components/AppPagination";
 import useProducts from "../../app/hooks/useProducts";
-import { Box ,Grid, Paper  } from "@mui/material";
+import { Box ,FormControl,Grid, InputLabel, MenuItem, Paper, Select, SelectChangeEvent  } from "@mui/material";
+import { useState } from "react";
+import React from "react";
 
 const sortOptions = [
     {value:'name', label: 'Alphabetical'},
@@ -15,42 +17,76 @@ const sortOptions = [
     {value:'priceAsc', label: 'Price - Low to High'},
 ]
 
+
+
 export default function Catalog() {
     const {products,brands, types, filtersLoaded,metaData} = useProducts();
     const{productParams}= useAppSelector(state=> state.catalog)
     const dispatch = useAppDispatch();
-    
+
 
 if(!filtersLoaded) return <LoadingComponent message="Loading Products..."/>
-
-/***            <Grid item xs={3}>
-          
-            <Paper sx={{mb: 2, p: 2}}>
-           <RadioButtonGroup
-           selectedValue={productParams.orderBy}
-           options={sortOptions}
-           onChange={(e)=> dispatch(setProductParams({orderBy: e.target.value}))}
-           />
-            </Paper>
-            <Paper sx={{mb: 2, p: 2}}>
-    <CheckboxButtons
-    items={brands}
-    checked={productParams.brands}
-    onChange={(items: string[]) => dispatch(setProductParams({brands: items}))}
-    />
-</Paper>
-            <Paper sx={{mb: 2, p: 2}}>
-            <CheckboxButtons
-    items={types}
-    checked={productParams.types}
-    onChange={(items: string[]) => dispatch(setProductParams({types: items}))}
-    />
-</Paper>
-
- */
-
+  
     return(
         <>
+<Box>
+  <Grid container spacing={2}>
+    <Grid item xs={12} sm={6} md={4}>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="OrderBy">Order By</InputLabel>
+        <Select
+          labelId="OrderBy"
+          id="OrderBy"
+          value={productParams.orderBy}
+          onChange={(e)=> dispatch(setProductParams({orderBy: e.target.value}))}
+          autoWidth
+          label="Order By"
+        >
+            {sortOptions.map(({value,label})=> (
+                <MenuItem value={value}>{label}</MenuItem>
+            ))}
+        </Select>
+      </FormControl> 
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="Brands">Brands</InputLabel>
+        <Select
+          labelId="Brands"
+          id="Brands"
+          value={productParams.brands}
+          onChange={(items) => dispatch(setProductParams({brands: items.target.value}))}
+          autoWidth
+          label="Brands"
+        >
+            <MenuItem value=""><em>None</em></MenuItem>
+            {brands.map((item)=> (
+                <MenuItem value={item}>{item}</MenuItem>
+            ))}
+        </Select>
+      </FormControl>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="types">Types</InputLabel>
+        <Select
+          labelId="types"
+          id="types"
+          value={productParams.types}
+          onChange={(items) => dispatch(setProductParams({types: items.target.value}))}
+          autoWidth
+          label="types"
+        >
+            <MenuItem value=""><em>None</em></MenuItem>
+            {types.map((item)=> (
+                <MenuItem value={item}>{item}</MenuItem>
+            ))}
+        </Select>
+      </FormControl>  
+    </Grid>
+  </Grid>
+</Box>
+
        
                 <Box sx={{ flexGrow: 1 }}> 
                 <ProductList products={products}></ProductList>
