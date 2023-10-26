99ed584eb6cdf34a5bff11cea19f62cb491528b6
andreshernandez92 <ajhl1992@gmail.com>
2023-10-25 23:51:40 -0600

    Homepage update beginning

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 export default function HomePage(){
 return(
     <Typography variant='h2'>
-        Home Page
+We are working on the  Homeopage
     </Typography>
 
 )
